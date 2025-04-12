ac8bb1a482dd8cb3f4f4fd856697673a00da10f6
KYUNG TAE <kkobe1228@gmail.com>
2025-04-12 16:14:33 +0900

    commit

---
+++ src/test/java/hello/buildtest/BuildTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.buildtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
